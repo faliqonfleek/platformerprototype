c646a2daaa4097f0eac2ba5696c070944526d8d7
fliqie <faliqtan@outlook.com>
2024-10-21 20:49:52 +0800

    Changes: 1. Animated enemy prefab
    Notes:
    - Few more enemy adjustments to make soon

--- Assets/Scripts/AnimationStrings.cs
+++ Assets/Scripts/AnimationStrings.cs
@@ -16,6 +16,7 @@ internal class AnimationStrings
     internal static string isOnWall = "IsOnWall";
     internal static string isOnCeiling = "IsOnCeiling";
     internal static string attackTrigger = "attack";
-    internal static string canMove = "canMove";
+    internal static string canMove = "CanMove";
+    internal static string hasTarget = "HasTarget";
 }
 
