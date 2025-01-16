4f3ad3a184ca61cbf6e0c686cef518ec14176731
fliqie <faliqtan@outlook.com>
2025-01-16 22:45:01 +0800

    Changes: 1. Added combo attack for player 2. Minor adjustments to camera 3. Text indicator readjustment
    Notes:
    - Attacks now hold up to 3 to do a combo
    - Camera adjusted to be slightly above ground
    - Text indicator now is smaller

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -8,7 +8,8 @@ using UnityEngine.Experimental.AI;
 [RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections), typeof(Damageable))]
 public class Knight : MonoBehaviour
 {
-    public float walkSpeed = 3f;
+    public float walkAcceleration = 3f;
+    public float maxSpeed = 3f;
     public DetectionZone attackZone;
     public float walkStopRate = 0.05f;
     public DetectionZone cliffDetectionZone;
@@ -111,7 +112,8 @@ public class Knight : MonoBehaviour
         if (!damageable.LockVelocity)
         {
             if (CanMove)
-                rb.velocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.velocity.y);
+                // Accelerate towards max speed
+                rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x + (walkAcceleration * walkDirectionVector.x * Time.fixedDeltaTime), -maxSpeed, maxSpeed), rb.velocity.y);
             else
                 rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, walkStopRate), rb.velocity.y);
         }
